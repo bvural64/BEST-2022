17230865c322a820a0ea08499a0fd9f5fda71c51
blue <58010007+blue64x@users.noreply.github.com>
2022-10-12 16:31:01 -0500

    init file structure

---
+++ src/arms.c
@@ -0,0 +1,9 @@
+// pragma statements will go here
+
+#include <stdio.h>
+// Include statements from other files will go here
+
+task moveArms() // function name subject to change, more functions could also be needed 
+{
+
+}
