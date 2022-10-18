f23b8b850da19190b9fbd5759f68292a1b31cb6d
baran <58010007+bvural64@users.noreply.github.com>
2022-10-18 12:19:06 -0500

    Drive Train Control + Future Arcade Control

--- MovementScript.c
+++ MovementScript.c
@@ -5,8 +5,41 @@
 
 task main()
 {
-	while (1 == 1){
-		motor[leftMotor] = vexRT[Ch3]; // left joystick y value
-		motor[rightMotor] = vexRT[Ch2]; // right joystick y value
-	}
+	// Initialize User Control using Infinite Loop
+	while (1 == 1)
+	{
+		/* 
+			Motor Power Values (-127 to 127 inclusive) are set to the values of the joysticks 
+			in a tank configuration to control drive train
+		*/
+		motor[leftMotor] = vexRT[Ch3]; // Left Joystick Y value 
+		motor[rightMotor] = vexRT[Ch2]; // Right Joystick Y value
+
+		/*
+
+		// Future Drive Train Code (Arcade Control)
+
+		// Turn Left
+		if (vexRT[Ch4] < 0) // Could flip, we dont know if left is negative or positive
+		{
+			motor[leftMotor] = vexRT[Ch4];
+			motor[rightMotor] = vexRT[Ch4] * -1;
+		}
+
+		// Turn Right
+		if (vexRT[Ch4] > 0) // Could flip, we dont know if right is negative or positive
+		{
+			motor[leftMotor] = vexRT[Ch4] * -1;
+			motor[rightMotor] = vexRT[Ch4];
+		}
+
+		// Forward/Backward Control
+		if (vexRT[Ch2]) 
+		{
+			motor[leftMotor] = vexRT[Ch2];
+			motor[rightMotor] = vexRT[Ch2];
+		}
+		
+		*/
+	} 
 }
